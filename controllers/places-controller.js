d15f69ac7fe833703d53e2eac91903d0d954d8be
sochubert <sochubert@gmail.com>
2022-05-16 12:21:21 +0900

    Creating &* Storing Places

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -2,6 +2,7 @@ const uuid = require("uuid").v4;
 const { validationResult } = require("express-validator");
 const HttpError = require("../models/http-error");
 const getCoordsForAddress = require("../util/location");
+const Place = require("../models/place");
 
 let DUMMY_PLACES = [
   {
@@ -62,16 +63,24 @@ const createPlace = async (req, res, next) => {
     return next(error);
   }
 
-  const createdPlace = {
-    id: uuid(),
-    title,
+  const createdPlace = new Place({
+    title: title,
     description,
-    location: coordinates,
     address,
+    location: coordinates,
+    image:
+      "https://www.akamai.com/site/im-demo/perceptual-standard.jpg?imbypass=true",
     creator,
-  };
-
-  DUMMY_PLACES.push(createPlace);
+  });
+  try {
+    await createdPlace.save();
+  } catch (err) {
+    const error = new HttpError(
+      "Creating place failed, please try again.",
+      500
+    );
+    return next(error);
+  }
 
   res.status(201).json({ place: createdPlace });
 };
