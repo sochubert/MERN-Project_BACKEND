9e8156c68b9b395684e542677b4bee1c03ccca73
sochubert <sochubert@gmail.com>
2022-05-16 12:09:30 +0900

    Connect MongoDB & Create Schema

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
@@ -25,4 +26,13 @@ app.use((error, req, res, next) => {
   res.json({ message: error.message || "An unkown error occurred!" });
 });
 
-app.listen(5001);
+mongoose
+  .connect(
+    "mongodb+srv://sochubert:hellomongodb@cluster0.6yrkg.mongodb.net/places?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(5001);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
