49274bca82a65b481985d98eab4e74501d6a0cf0
sochubert <sochubert@gmail.com>
2022-05-18 16:03:07 +0900

    Access auth

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Acess-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
@@ -28,7 +38,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://sochubert:hellomongodb@cluster0.6yrkg.mongodb.net/places?retryWrites=true&w=majority"
+    "mongodb+srv://sochubert:hellomongodb@cluster0.6yrkg.mongodb.net/mern?retryWrites=true&w=majority"
   )
   .then(() => {
     app.listen(5001);
